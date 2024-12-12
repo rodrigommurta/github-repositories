ce095f03bd69bf86ed7e671a637c4823af983d08
Rodrigo Murta <rodrigommurta@gmail.com>
2024-12-12 00:27:18 -0300

    feat: utils files

---
+++ modules/domain/src/main/java/com/murta/github_repositories/domain/utils/State.kt
@@ -0,0 +1,41 @@
+package com.murta.github_repositories.domain.utils
+
+sealed class State<T>(
+    open val data: T? = null,
+    open val error: ErrorInformation? = null,
+) {
+    data class Loading<T>(
+        override val data: T? = null,
+        override val error: ErrorInformation? = null,
+    ) : State<T>()
+
+    data class Success<T>(override val data: T?) : State<T>(data = data)
+
+    data class Error<T>(
+        override val data: T? = null,
+        override val error: ErrorInformation,
+    ) : State<T>(null, error)
+
+    val isLoading get() = this is Loading
+    val isNotLoading get() = isLoading.not()
+    val isSuccess get() = this is Success
+    val isNotSuccess get() = isSuccess.not()
+    val isError get() = this is Error
+    val isNotError get() = isError.not()
+}
+
+data class ErrorInformation(
+    val message: String? = null,
+    val cause: Throwable? = null,
+)
+
+fun Exception.toErrorInformation(message: String? = null) = ErrorInformation(
+    message = message
+        ?: localizedMessage
+        ?: "Ocorreu um erro por aqui, tente novamente mais tarde.",
+    cause = cause
+)
+
+interface StateBearer {
+    val state: State<Any>
+}
---
+++ modules/domain/src/main/java/com/murta/github_repositories/domain/utils/UseCase.kt
@@ -0,0 +1,15 @@
+package com.murta.github_repositories.domain.utils
+
+import kotlinx.coroutines.flow.Flow
+
+abstract class UseCase<K, T> {
+    abstract suspend fun execute(
+        param: K,
+        currentState: T,
+    ): Flow<T>
+
+    suspend operator fun invoke(
+        param: K,
+        currentState: T
+    ) = execute(param, currentState)
+}
