87009e093d07cdb0193a0242010dc7a344021b93
Rodrigo Murta <rodrigommurta@gmail.com>
2024-12-12 00:21:58 -0300

    feat: init navigation impl

--- modules/app/src/main/java/com/murta/github_repositories/MainActivity.kt
+++ modules/app/src/main/java/com/murta/github_repositories/MainActivity.kt
@@ -11,19 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.murta.github_repositories.ui.theme.GithubrepositoriesTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.murta.github_repositories.ui.theme.GitHubRepositoriesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GithubrepositoriesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            GitHubRepositoriesTheme {
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("main") {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier
+                        )
+                    }
                 }
             }
         }
@@ -32,16 +39,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(innerPadding)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GithubrepositoriesTheme {
+    GitHubRepositoriesTheme {
         Greeting("Android")
     }
 }
--- modules/app/src/main/java/com/murta/github_repositories/ui/theme/Theme.kt
+++ modules/app/src/main/java/com/murta/github_repositories/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun GithubrepositoriesTheme(
+fun GitHubRepositoriesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
