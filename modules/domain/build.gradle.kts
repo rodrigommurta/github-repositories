5b34085fb577fe7f6b74099dfdc946107b779326
Rodrigo Murta <rodrigommurta@gmail.com>
2024-12-12 00:25:33 -0300

    feat: create domain module

---
+++ modules/domain/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+//    testImplementation(libs.kotlinx.coroutines.test)
+}
