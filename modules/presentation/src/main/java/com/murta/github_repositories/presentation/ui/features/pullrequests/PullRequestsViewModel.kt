a6c7eba78b00f5d922711ec47c852c449ec5136d
Rodrigo Murta <rodrigommurta@gmail.com>
2024-12-15 12:12:18 -0300

    feat: update viewmodels

--- modules/presentation/src/main/java/com/murta/github_repositories/presentation/ui/features/pullrequests/PullRequestsViewModel.kt
+++ modules/presentation/src/main/java/com/murta/github_repositories/presentation/ui/features/pullrequests/PullRequestsViewModel.kt
@@ -3,44 +3,52 @@ package com.murta.github_repositories.presentation.ui.features.pullrequests
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
 import com.murta.github_repositories.domain.features.pullrequests.GetPullRequestsUseCase
 import com.murta.github_repositories.domain.features.pullrequests.model.PullRequestsScreen
+import com.murta.github_repositories.presentation.di.ViewModelAssistedFactory
+import com.murta.github_repositories.presentation.ui.navigation.PullRequestsRoute
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-const val PULL_REQUESTS_STATE = "PULL_REQUESTS_STATE"
+private const val TITLE = "title"
+private const val FULL_NAME = "fullName"
 
-class PullRequestsViewModel(
+class PullRequestsViewModel @AssistedInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle,
     private val getPullRequestsUseCase: GetPullRequestsUseCase,
-    private val savedState: SavedStateHandle,
 ) : ViewModel() {
     private val _screen = MutableStateFlow(PullRequestsScreen())
     val screen: StateFlow<PullRequestsScreen> = _screen.asStateFlow()
 
+    private val route = savedStateHandle.toRoute<PullRequestsRoute>()
+
     init {
-        if (savedState.contains(PULL_REQUESTS_STATE).not()) {
-            getData()
-        } else {
-            savedState.get<PullRequestsScreen>(PULL_REQUESTS_STATE)?.let {
-                viewModelScope.launch {
-                    _screen.emit(it)
-                }
-            }
-        }
+        getData(route)
     }
 
-    private fun getData() {
+    fun getData(route: PullRequestsRoute) {
+        val mapParams = mapOf(
+            TITLE to route.title.orEmpty(),
+            FULL_NAME to route.fullName.orEmpty(),
+        )
+
         viewModelScope.launch {
-            getPullRequestsUseCase(null, screen.value)
-                .onEach {
-                    savedState[PULL_REQUESTS_STATE] = it
-                }
+            getPullRequestsUseCase(
+                params = mapParams,
+                currentState = screen.value,
+            )
                 .collect {
                     _screen.emit(it)
                 }
         }
     }
+
+    @AssistedFactory
+    interface Factory : ViewModelAssistedFactory<PullRequestsViewModel>
 }
--- modules/presentation/src/main/java/com/murta/github_repositories/presentation/ui/features/repositories/RepositoriesViewModel.kt
+++ modules/presentation/src/main/java/com/murta/github_repositories/presentation/ui/features/repositories/RepositoriesViewModel.kt
@@ -5,42 +5,35 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.murta.github_repositories.domain.features.repositories.GetRepositoriesUseCase
 import com.murta.github_repositories.domain.features.repositories.model.RepositoriesScreen
+import com.murta.github_repositories.presentation.di.ViewModelAssistedFactory
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-const val REPOSITORIES_STATE = "REPOSITORIES_STATE"
-
-class RepositoriesViewModel(
+class RepositoriesViewModel @AssistedInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle,
     private val getRepositoriesUseCase: GetRepositoriesUseCase,
-    private val savedState: SavedStateHandle,
 ) : ViewModel() {
     private val _screen = MutableStateFlow(RepositoriesScreen())
     val screen: StateFlow<RepositoriesScreen> = _screen.asStateFlow()
 
     init {
-        if (savedState.contains(REPOSITORIES_STATE).not()) {
-            getData()
-        } else {
-            savedState.get<RepositoriesScreen>(REPOSITORIES_STATE)?.let {
-                viewModelScope.launch {
-                    _screen.emit(it)
-                }
-            }
-        }
+        getData()
     }
 
-    private fun getData() {
+    fun getData() {
         viewModelScope.launch {
             getRepositoriesUseCase(null, screen.value)
-                .onEach {
-                    savedState[REPOSITORIES_STATE] = it
-                }
                 .collect {
                     _screen.emit(it)
                 }
         }
     }
+
+    @AssistedFactory
+    interface Factory : ViewModelAssistedFactory<RepositoriesViewModel>
 }
